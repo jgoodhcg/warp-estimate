f5d267fb4b8a24ed4f813379b4dee05dd18eafbb
Justin <jgoodhcg@gmail.com>
2023-04-26 19:57:22 -0400

    Working round trip ws

---
+++ src/clj/jgood/warp_estimate/db.clj
@@ -0,0 +1,1 @@
+(ns jgood.warp-estimate.db)
--- src/clj/jgood/warp_estimate/web/routes/api.clj
+++ src/clj/jgood/warp_estimate/web/routes/api.clj
@@ -1,14 +1,20 @@
 (ns jgood.warp-estimate.web.routes.api
   (:require
-    [jgood.warp-estimate.web.controllers.health :as health]
-    [jgood.warp-estimate.web.middleware.exception :as exception]
-    [jgood.warp-estimate.web.middleware.formats :as formats]
-    [integrant.core :as ig]
-    [reitit.coercion.malli :as malli]
-    [reitit.ring.coercion :as coercion]
-    [reitit.ring.middleware.muuntaja :as muuntaja]
-    [reitit.ring.middleware.parameters :as parameters]
-    [reitit.swagger :as swagger]))
+   [hiccup.core :refer [html]]
+   [hiccup.page :refer [html5]]
+   [integrant.core :as ig]
+   [jgood.warp-estimate.web.controllers.health :as health]
+   [jgood.warp-estimate.web.middleware.exception :as exception]
+   [jgood.warp-estimate.web.middleware.formats :as formats]
+   [reitit.coercion.malli :as malli]
+   [reitit.ring.coercion :as coercion]
+   [reitit.ring.middleware.muuntaja :as muuntaja]
+   [reitit.ring.middleware.parameters :as parameters]
+   [reitit.swagger :as swagger]
+   [ring.adapter.undertow.websocket :as ws]
+   [ring.util.http-response :as http-response]
+   [clojure.pprint :refer [pprint]]
+   [cheshire.core :as json]))
 
 (def route-data
   {:coercion   malli/coercion
@@ -31,6 +37,26 @@
                   ;; exception handling
                 exception/wrap-exception]})
 
+(defn htmx-page [body]
+  (-> (html5
+       {:lang "en"}
+       [:head
+        [:meta {:charset "UTF-8"}]
+        [:meta {:name "viewport" :content "width=device-width, initial-scale=1.0"}]
+        [:script {:src "https://cdn.tailwindcss.com"}]
+        [:script {:src "https://unpkg.com/htmx.org@1.8.6"}]
+        [:script {:src "https://unpkg.com/htmx.org/dist/ext/ws.js"}]]
+
+       [:body.bg-gradient-to-b.from-indigo-800.to-indigo-950.min-h-screen.px-4.pt-4
+        body])
+      http-response/ok
+      (http-response/content-type "text/html")))
+
+(defn htmx-component
+  [body]
+  (-> body html http-response/ok (http-response/content-type "text/html")))
+
+
 ;; Routes
 (defn api-routes [_opts]
   [["/swagger.json"
@@ -38,7 +64,31 @@
            :swagger {:info {:title "jgood.warp-estimate API"}}
            :handler (swagger/create-swagger-handler)}}]
    ["/health"
-    {:get health/healthcheck!}]])
+    {:get health/healthcheck!}]
+
+   ["/"
+    (fn [_] (-> [:div.bg-slate-200.rounded.p-4 {:hx-ext "ws" :ws-connect "/api/ws-test"}
+                [:div {:id "echo"}]
+                [:form {:id "form" :ws-send true}
+                 [:input.p-2.rounded {:name "msg"}]]]
+
+               htmx-page))]
+   ["/tmp"
+    (fn [_] (-> [:div "tmp"] htmx-component))]
+   ["/ws-test"
+    (fn [request]
+      {:undertow/websocket
+       {:on-open          (fn [{:keys [channel]}] (println "WS open!"))
+        :on-message       (fn [{:keys [channel data]}]
+                            (let [data (json/parse-string data true)]
+                              (pprint {:location :on-message
+                                       :data data})
+                              (ws/send
+                               (-> [:div {:id "echo"} (:msg data)]
+                                   html)
+                               channel)))
+        :on-close-message (fn [{:keys [channel message]}] (println "WS closeed!"))}})]
+   ])
 
 (derive :reitit.routes/api :reitit/routes)
 
